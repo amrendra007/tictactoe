fe10875bde5434a0be3815438d6b60104aff27eb
amrendra007 <amrendrapcs07@gmail.com>
2018-07-08 15:04:41 +0530

    code refactoring

--- README.md
+++ README.md
@@ -1,2 +1,20 @@
-# tictactoe
-https://amrendra007.github.io/tictactoe/
+# Tic-Tac-Toe unbeatable
+---
+As a part of learning Javascript, i have created this project.
+Its a popular tic-tac-toe game, taking observation of how game 
+works i have made this game unbeatable.
+
+### Algorithm 
+
+First turn is given to human(x) player then system(0) takes its turn.
+
+Before taking any move, system always check for two adjacent 0 on board and if its find one it will mark 0 there.
+I call this system attacking strategy to take over the game.
+
+If attacking is not the case then system will check for two adjacent x on board and in case if its find such pair
+it will mark 0 there, i call this system defending strategy.
+
+And in rest cases, system will take some intelligent move according to how user is playing
+
+
+Live here <https://amrendra007.github.io/tictactoe/>
--- script.js
+++ script.js
@@ -9,7 +9,6 @@ var commonBlocker = 0;
 var clickPreventer = 0;
 var diagonalDeadZone1 = 0;
 var diagonalDeadZone2 = 0;
-
 var AI = document.getElementById("AI");
 var HUMAN = document.getElementById("HUMAN");
 var toggle = document.getElementById("toggle");
@@ -24,19 +23,15 @@ var array = [ ];
 function random(min,max) {
 	return Math.floor(Math.random()*(max -min) + min);
 }
-
 for(let i = 0; i < blk.length; i++) {
 	blk[i].addEventListener("click",inIt);
 }
-
 function inIt() {
 	if(this.value !== "x" && this.value === " " && gameOver === false && clickPreventer === 0) {
 		this.value = "0";
         clickPreventer = 1;
-
         //cheacking human wining condition
 		if(winningCombination("0")) {
-
 			gameOver = true;
             humanScore += 1;
 			HUMAN.textContent = "YOU(O) - " + humanScore;
@@ -44,359 +39,264 @@ function inIt() {
             updateMessage.textContent = "Congratulation ! you won.";
             setTimeout(resetGame,1500);
         }
-
 		//checking for game draw
 		else if (vacantChecker() && !winningCombination("0") && !winningCombination("x") ) {
-
 			gameOver = true;
             toggle.style.display = "block";
             updateMessage.textContent = "Well tried, It's draw.";
             setTimeout(resetGame,1500);
         }
-
 		if(gameOver === false) {
 			AIplay();
 		}
     }
 
 }
-
-function vacantChecker() {
-
-	for(let i = 0 ; i < blk.length; i++) {
-		if(blk[i].value === " ") {
-			return false;
-		}
-	}
-
-	return true;
-}
-
-function winningCombination(bit) {
-
-	//checking horizontally
-	if(blk[0].value === bit && blk[1].value === bit && blk[2].value === bit) {
-		blk[0].style.color = "green";
-		blk[1].style.color = "green";
-		blk[2].style.color = "green";
-	}
-
-	else if(blk[3].value === bit && blk[4].value === bit && blk[5].value === bit) {
-		blk[3].style.color = "green";
-		blk[4].style.color = "green";
-		blk[5].style.color = "green";
-	}
-
-	else if(blk[6].value === bit && blk[7].value === bit && blk[8].value === bit) {
-		blk[6].style.color = "green";
-		blk[7].style.color = "green";
-		blk[8].style.color = "green";
-	}
-
-	//checking vertically
-	else if(blk[0].value === bit && blk[3].value === bit && blk[6].value === bit) {
-		blk[0].style.color = "green";
-		blk[3].style.color = "green";
-		blk[6].style.color = "green";
-	}
-
-	else if(blk[1].value === bit && blk[4].value === bit && blk[7].value === bit) {
-		blk[1].style.color = "green";
-		blk[4].style.color = "green";
-		blk[7].style.color = "green";
-	}
-
-	else if(blk[2].value === bit && blk[5].value === bit && blk[8].value === bit) {
-		blk[2].style.color = "green";
-		blk[5].style.color = "green";
-		blk[8].style.color = "green";
-	}
-
-	//checking diagonally
-	else if(blk[0].value === bit && blk[4].value === bit && blk[8].value === bit) {
-		blk[0].style.color = "green";
-		blk[4].style.color = "green";
-		blk[8].style.color = "green";
-	}
-
-	else if(blk[6].value === bit && blk[4].value === bit && blk[2].value === bit) {
-		blk[6].style.color = "green";
-		blk[4].style.color = "green";
-		blk[2].style.color = "green";
-	}
-
-	else {
-		return false;
-	}
-
-	return true;
-
-}
-
 function AIplay() {
-
 	defendChecker = "undefend";
-
-	if(firstTurn === 0 && blk[4].value === " ") {
-		wraper(blk[4]);
+	if (firstTurn === 0 && blk[4].value === " ") {
+		markAndCheck(blk[4]);
 		firstTurn = 1;
-		
 	} else {
 		firstTurn = 2;
 	}
-
-    //when user select middle box , how ai respond intell.
-	if(userMiddleTurn === 0 && blk[4].value === "0" && blk[6].value === " ") {
-		wraper(blk[6]);
+	//when user select middle box , how ai respond intell.
+	if (userMiddleTurn === 0 && blk[4].value === "0" && blk[6].value === " ") {
+		markAndCheck(blk[6]);
 		userMiddleTurn = 1;
-
 	} else {
 		userMiddleTurn = 2;
 	}
-
 	//Attacking code , check for two "x" and mark third if find
-	if(attackOrDefend("x")) {
+	if (attackOrDefend("x")) {
 		attackChecker = "attacked";
 	}
-
 	//Defending code , check for two "0" and mark third if find
-	if( attackChecker !== "attacked" && attackOrDefend("0") ) {
+	if (attackChecker !== "attacked" && attackOrDefend("0")) {
 		defendChecker = "defended";
 	}
 
-    //============diagonalldeadzone1======================
-    if(blk[6].value === "0" && blk[2].value === "0" && blk[4].value === "x" && blk[0].value === " " &&
-        blk[1].value === " " && blk[3].value === " " && blk[5].value === " " && blk[7].value === " " &&
-        blk[8].value === " ") {
+	//============diagonalldeadzone1======================
+	if (blk[6].value === "0" && blk[2].value === "0" && blk[4].value === "x" && blk[0].value === " " && blk[1].value === " " && blk[3].value === " " && blk[5].value === " " && blk[7].value === " " && blk[8].value === " ") {
+		markAndCheck(blk[5]);
+		diagonalDeadZone1 = 1;
+	} else {
+		diagonalDeadZone1 = 2;
+	}
 
-        wraper(blk[5]);
-        diagonalDeadZone1 = 1;
+	//=============diagonal deadzone2================
+	if (blk[0].value === "0" && blk[8].value === "0" && blk[4].value === "x" && blk[1].value === " " && blk[2].value === " " && blk[3].value === " " && blk[5].value === " " && blk[6].value === " " && blk[7].value === " ") {
+		markAndCheck(blk[3]);
+		diagonalDeadZone2 = 1;
+	} else {
+		diagonalDeadZone2 = 2;
+	}
 
-    } else {
-        diagonalDeadZone1 = 2;
-    }
-
-    //=============diagonal deadzone2================
-    if(blk[0].value === "0" && blk[8].value === "0" && blk[4].value === "x" && blk[1].value === " " &&
-        blk[2].value === " " && blk[3].value === " " && blk[5].value === " " && blk[6].value === " " &&
-        blk[7].value === " ") {
-
-        wraper(blk[3]);
-        diagonalDeadZone2 = 1;
-
-    } else {
-        diagonalDeadZone2 = 2;
-    }
-
-    //Deadzonemiddle condition x on 6 and 0 on pos 4 , 2
-	if(blk[6].value === "x" && blk[4].value === "0" && blk[2].value === "0" && blk[0].value === " " &&
-	 blk[1].value === " " && blk[3].value === " " && blk[5].value === " " && blk[7].value === " " &&
-	 blk[8].value === " ") {
-
-		wraper(blk[0]);
+	//Deadzonemiddle condition x on 6 and 0 on pos 4 , 2
+	if (blk[6].value === "x" && blk[4].value === "0" && blk[2].value === "0" && blk[0].value === " " && blk[1].value === " " && blk[3].value === " " && blk[5].value === " " && blk[7].value === " " && blk[8].value === " ") {
+		markAndCheck(blk[0]);
 		deadZoneTurn = 1;
-
 	} else {
 		deadZoneTurn = 2;
 	}
 
 	//===========================Running code for vacant position====================
-	if(defendChecker === "undefend" && attackChecker !== "attacked" && (firstTurn === 2 || firstTurn != 1) &&
-	(userMiddleTurn === 2 || userMiddleTurn != 1)&& (deadZoneTurn === 2 || deadZoneTurn != 1) &&
-    (diagonalDeadZone1 === 2 || diagonalDeadZone1 != 1) && (diagonalDeadZone2 === 2 || diagonalDeadZone2 != 1))
-    {
-
+	if (defendChecker === "undefend" && attackChecker !== "attacked" && (firstTurn === 2 || firstTurn != 1) && (userMiddleTurn === 2 || userMiddleTurn != 1) && (deadZoneTurn === 2 || deadZoneTurn != 1) && (diagonalDeadZone1 === 2 || diagonalDeadZone1 != 1) && (diagonalDeadZone2 === 2 || diagonalDeadZone2 != 1)) {
 		//======blocking common position created by user side by side======
-        if (commonBlocker === 0 && blk[6].value === " " && (blk[0].value === "0" || blk[3].value === "0") &&
-		 (blk[7].value === "0" || blk[8].value === "0") ) {
-			 commonBlocker = 1;
-			 wraper(blk[6]);
+		if (commonBlocker === 0 && blk[6].value === " " && (blk[0].value === "0" || blk[3].value === "0") && (blk[7].value === "0" || blk[8].value === "0")) {
+			commonBlocker = 1;
+			markAndCheck(blk[6]);
 
 		}
-
-		else if (commonBlocker === 0 && blk[8].value === " " && (blk[6].value === "0" || blk[7].value === "0") &&
-			(blk[5].value === "0" || blk[2].value === "0")) {
+		else if (commonBlocker === 0 && blk[8].value === " " && (blk[6].value === "0" || blk[7].value === "0") && (blk[5].value === "0" || blk[2].value === "0")) {
 			commonBlocker = 1;
-			wraper(blk[8]);
+			markAndCheck(blk[8]);
+		}
+		else if (commonBlocker === 0 && blk[2].value === " " && (blk[8].value === "0" || blk[5].value === "0") && (blk[0].value === "0" || blk[1].value === "0")) {
+			commonBlocker = 1;
+			markAndCheck(blk[2]);
 
 		}
-
-		else if (commonBlocker === 0 && blk[2].value === " " && (blk[8].value === "0" || blk[5].value === "0") &&
-			(blk[0].value === "0" || blk[1].value === "0")) {
+		else if (commonBlocker === 0 && blk[0].value === " " && (blk[1].value === "0" || blk[2].value === "0") && (blk[3].value === "0" || blk[6].value === "0")) {
 			commonBlocker = 1;
-			wraper(blk[2]);
-
+			markAndCheck(blk[0]);
 		}
-
-		else if (commonBlocker === 0 && blk[0].value === " " && (blk[1].value === "0" || blk[2].value === "0") &&
-			(blk[3].value === "0" || blk[6].value === "0")) {
-			commonBlocker = 1;
-			wraper(blk[0]);
-
-		} 
-		
 		else {
-
 			//marking on random pos
 			for (let i = 0; i < blk.length; i++) {
 				if (blk[i].value === " ") {
 					array.push(blk[i]);
 				}
 			}
-
 			//marking on random location
 			let vacantPos = random(0, array.length);
-			wraper(array[vacantPos]);
-        }
-
+			markAndCheck(array[vacantPos]);
+		}
 	}
-
-
 }
 
+function vacantChecker() {
+	for(let i = 0 ; i < blk.length; i++) {
+		if(blk[i].value === " ") {
+			return false;
+		}
+	}
+	return true;
+}
+
+function winningCombination(bit) {
+	//checking horizontally
+	if(blk[0].value === bit && blk[1].value === bit && blk[2].value === bit) {
+		colorGreen(blk[0], blk[1], blk[2]);
+	}
+	else if(blk[3].value === bit && blk[4].value === bit && blk[5].value === bit) {
+		colorGreen(blk[3], blk[4], blk[5]);
+	}
+	else if(blk[6].value === bit && blk[7].value === bit && blk[8].value === bit) {
+		colorGreen(blk[6], blk[7], blk[8]);
+	}
+	//checking vertically
+	else if(blk[0].value === bit && blk[3].value === bit && blk[6].value === bit) {
+		colorGreen(blk[0], blk[3], blk[6]);
+	}
+	else if(blk[1].value === bit && blk[4].value === bit && blk[7].value === bit) {
+		colorGreen(blk[1], blk[4], blk[7]);
+	}
+	else if(blk[2].value === bit && blk[5].value === bit && blk[8].value === bit) {
+		colorGreen(blk[2], blk[5], blk[8]);
+	}
+	//checking diagonally
+	else if(blk[0].value === bit && blk[4].value === bit && blk[8].value === bit) {
+		colorGreen(blk[0], blk[4], blk[8]);
+	}
+	else if(blk[6].value === bit && blk[4].value === bit && blk[2].value === bit) {
+		colorGreen(blk[6], blk[4], blk[2]);
+	}
+	else {
+		return false;
+	}
+	return true;
+}
+
+function colorGreen(blk1, blk2, blk3) {
+	blk1.style.color = 'green';
+	blk2.style.color = 'green';
+	blk3.style.color = 'green';
+}
 
 function attackOrDefend(bit) {
-
 	//==========================checking horizontall======================
-
 	//First hrline
 	if(blk[0].value === bit && blk[1].value === bit && blk[2].value === " ") {
-		wraper(blk[2]);
+		markAndCheck(blk[2]);
 	}
 	else if(blk[1].value === bit && blk[2].value === bit && blk[0].value === " ") {
-		wraper(blk[0]);
+		markAndCheck(blk[0]);
 	}
 	else if(blk[0].value === bit && blk[2].value === bit && blk[1].value === " ") {
-		wraper(blk[1]);
+		markAndCheck(blk[1]);
 	}
-
 	//Second hrline
 	else if(blk[3].value === bit && blk[4].value === bit && blk[5].value === " ") {
-		wraper(blk[5]);
+		markAndCheck(blk[5]);
 	}
 	else if(blk[4].value === bit && blk[5].value === bit && blk[3].value === " ") {
-		wraper(blk[3]);
+		markAndCheck(blk[3]);
 	}
 	else if(blk[3].value === bit && blk[5].value === bit && blk[4].value === " ") {
-		wraper(blk[4]);
+		markAndCheck(blk[4]);
 	}
-	
 	//Third hrline
 	else if(blk[6].value === bit && blk[7].value === bit && blk[8].value === " ") {
-		wraper(blk[8]);
+		markAndCheck(blk[8]);
 	}
 	else if(blk[7].value === bit && blk[8].value === bit && blk[6].value === " ") {
-		wraper(blk[6]);
+		markAndCheck(blk[6]);
 	}
 	else if(blk[6].value === bit && blk[8].value === bit && blk[7].value === " ") {
-		wraper(blk[7]);
+		markAndCheck(blk[7]);
 	}
-
-
 	//==========================checking vertically======================
-
 	//First vrline
 	else if(blk[0].value === bit && blk[3].value === bit && blk[6].value === " ") {
-		wraper(blk[6]);
+		markAndCheck(blk[6]);
 	}
 	else if(blk[3].value === bit && blk[6].value === bit && blk[0].value === " ") {
-		wraper(blk[0]);
+		markAndCheck(blk[0]);
 	}
 	else if(blk[0].value === bit && blk[6].value === bit && blk[3].value === " ") {
-		wraper(blk[3]);
+		markAndCheck(blk[3]);
 	}
-
-	//Second hrline
+	//Second vrline
 	else if(blk[1].value === bit && blk[4].value === bit && blk[7].value === " ") {
-		wraper(blk[7]);
+		markAndCheck(blk[7]);
 	}
 	else if(blk[4].value === bit && blk[7].value === bit && blk[1].value === " ") {
-		wraper(blk[1]);
+		markAndCheck(blk[1]);
 	}
 	else if(blk[1].value === bit && blk[7].value === bit && blk[4].value === " ") {
-		wraper(blk[4]);
+		markAndCheck(blk[4]);
 	}
-	
-	//Third hrline
+	//Third vrline
 	else if(blk[2].value === bit && blk[5].value === bit && blk[8].value === " ") {
-		wraper(blk[8]);
+		markAndCheck(blk[8]);
 	}
 	else if(blk[5].value === bit && blk[8].value === bit && blk[2].value === " ") {
-		wraper(blk[2]);
+		markAndCheck(blk[2]);
 	}
 	else if(blk[2].value === bit && blk[8].value === bit && blk[5].value === " ") {
-		wraper(blk[5]);
+		markAndCheck(blk[5]);
 	}
-
-
 	//==========================checking diagonally======================
-
 	//First dlline
 	else if(blk[0].value === bit && blk[4].value === bit && blk[8].value === " ") {
-		wraper(blk[8]);
+		markAndCheck(blk[8]);
 	}
 	else if(blk[4].value === bit && blk[8].value === bit && blk[0].value === " ") {
-		wraper(blk[0]);
+		markAndCheck(blk[0]);
 	}
 	else if(blk[0].value === bit && blk[8].value === bit && blk[4].value === " ") {
-		wraper(blk[4]);
+		markAndCheck(blk[4]);
 	}
-
 	//Second dlline
 	else if(blk[6].value === bit && blk[4].value === bit && blk[2].value === " ") {
-		wraper(blk[2]);
+		markAndCheck(blk[2]);
 	}
 	else if(blk[4].value === bit && blk[2].value === bit && blk[6].value === " ") {
-		wraper(blk[6]);
+		markAndCheck(blk[6]);
 	}
 	else if(blk[6].value === bit && blk[2].value === bit && blk[4].value === " ") {
-		wraper(blk[4]);
+		markAndCheck(blk[4]);
 	}
-
 	else{
 		return false;
 	}
-
 	return true;
-
 }
 
-function wraper(boardblock) {
+function markAndCheck(boardblock) {
 	setTimeout(function() {
 		boardblock.value = "x";
-
-
 		//checking ai winning condition
 		if(winningCombination("x")) {
 			AIScore += 1;
 			AI.textContent = "AI(X) - "+ AIScore;
 			gameOver = true;
-
 			toggle.style.display = "block";
-
 			updateMessage.textContent = "OOPS! you lost, try again.";
 			updateMessage.style.color = "red";
-
 			setTimeout(resetGame,1500);
 		}
-
-
 		//game draw status checking
 		else if (vacantChecker() && !winningCombination("0") && !winningCombination("x") ) {
 			gameOver = true;
-
 			toggle.style.display = "block";
-
 			updateMessage.textContent = "Well tried, It's draw.";
-
 			setTimeout(resetGame,1500);
-
 		}
         clickPreventer = 0;
-
 	}, 500);
 }
-
 
 function resetGame() {
 	defendChecker = "undefend";
@@ -412,10 +312,8 @@ function resetGame() {
 	toggle.style.display = "none";
 	updateMessage.style.color = "green";
 	array = [ ];
-
 	for(let i = 0; i < blk.length; i++) {
 		blk[i].value = " ";
 		blk[i].style.color = "white";
 	}
-
 }
